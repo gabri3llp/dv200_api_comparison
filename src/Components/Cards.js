992ccfc639f261ab95d90ee0030236730018324d
gabri3llp <251298@virtualwindow.co.za>
2026-03-10 22:36:50 +0200

    Getting started
    Got react working, added Navs and cards and FINALLY got the ccs to work

---
+++ src/Components/Cards.js
@@ -0,0 +1,21 @@
+import '../App.css';
+
+import Card from 'react-bootstrap/Card';
+
+function MyCards() {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Game</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Game info, eg Prce and name
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default MyCards;
